691dfb683eba585a5d6dc51b21ed692b09b98e5a
Adhitya Ramadhanus <adhitya.ramadhanus@gmail.com>
2016-09-18 21:24:36 +0700

    add cors handler

--- setup/express.js
+++ setup/express.js
@@ -2,6 +2,7 @@
 const path = require('path')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
+const cors = require('express-cors')
 const loader = require('../helpers/loader').loadRoute
 
 module.exports = (app, express) => {
@@ -12,6 +13,11 @@ module.exports = (app, express) => {
   // Middlewares setup
   app.use(logger('dev'))
 
+  app.use(cors({
+    allowedOrigins: [
+        'http://localhost:9966'
+    ]
+  }))
   // App Routes
   loader(path.join(__dirname, '..', 'routes'), app)
 }
