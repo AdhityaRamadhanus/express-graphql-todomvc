36fde035c2deadea0f6e8947113889128c725a46
Adhitya Ramadhanus <adhitya.ramadhanus@gmail.com>
2016-09-18 13:17:10 +0700

    add more mutations, add new dependency

--- helpers/mongoresolver.js
+++ helpers/mongoresolver.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Todo = mongoose.model('Todo')
+const async = require('async')
 
 module.exports.getAll = (roots, args, req) => {
 	return new Promise((resolve, reject) => {
@@ -29,5 +30,44 @@ module.exports.deleteTodo = (roots, args, req) => {
     else if (!removedTodo) reject(new Error('Todos Not Found!'))
     else resolve(removedTodo)
    }) 
+  })
+}
+
+module.exports.toggleOne = (roots, args, req) => {
+  return new Promise((resolve, reject) => {
+    Todo.findByIdAndUpdate(args.id, {completed: true}, {new: true}, (err, updatedTodo) => {
+      if (err) reject(err)
+      else resolve(updatedTodo)
+    })
+  })
+}
+
+module.exports.toggleAll = (roots, args, req) => {
+  return new Promise((resolve, reject) => {
+    async.waterfall([
+      (cb) => {
+        Todo.update({}, {completed: true}, {multi: true}, (err, response) => {
+          cb(err)
+        })
+      },
+      (cb) => {
+        Todo.find({}, (err, todos) => {
+          cb(err, todos)
+        })
+      }
+    ], (err, todos) => {
+        if (err) reject(err)
+        else resolve(todos)
+      }
+    )
+  })
+}
+
+module.exports.saveTodo = (roots, args, req) => {
+  return new Promise((resolve, reject) => {
+    Todo.findByIdAndUpdate(args.id, {title: args.title}, {new: true}, (err, updatedTodo) => {
+      if (err) reject(err)
+      else resolve(updatedTodo)
+    })
   })
 }
--- models/graphql.js
+++ models/graphql.js
@@ -49,11 +49,48 @@ var MutationDelete = {
   resolve: mongoResolver.deleteTodo
 }
 
+var MutationToggleOne = {
+  type: TodoType,
+  description: 'Toggle one todos to completed',
+  args: {
+    id: {
+      name: 'Todo id',
+      type: graphql.GraphQLID
+    }
+  },
+  resolve: mongoResolver.toggleOne
+}
+
+var MutationToggleAll = {
+  type: new graphql.GraphQLList(TodoType),
+  description: 'Toggle all todos to completed',
+  resolve: mongoResolver.toggleAll
+}
+
+var MutationUpdate = {
+  type: TodoType,
+  description: 'Update a todo',
+  args: {
+    id: {
+      name: 'Todo id',
+      type: graphql.GraphQLID
+    },
+    title: {
+      name: 'Todo title',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+    }
+  },
+  resolve: mongoResolver.saveTodo
+}
+
 var MutationType = new graphql.GraphQLObjectType({  
   name: 'Mutation',
   fields: {
     add: MutationAdd,
-    delete: MutationDelete
+    delete: MutationDelete,
+    toggleOne: MutationToggleOne,
+    toggleAll: MutationToggleAll,
+    update: MutationUpdate
   }
 })
 
